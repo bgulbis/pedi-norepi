20ddc0916cda6c2800fd3207a2aecb78725995db
Brian Gulbis <brian.gulbis@gmail.com>
2018-07-27 15:45:20 -0500

    work on finding patients

--- src/01_find-patients.R
+++ src/01_find-patients.R
@@ -13,3 +13,22 @@ id <- "millennium.id"
 #       - Medication (Generic): norepinephrine
 #       - Nurse Unit (Med): HC PICU;HC NICE;HC NICW;HC A8N4;HC A8NH
 
+potential_pts <- read_data(dir_raw, "patients", FALSE) %>%
+    as.patients()
+
+mbo_id <- concat_encounters(potential_pts$millennium.id)
+
+# run MBO queries
+#   * Clinical Events - No Order Id - Prompt
+#       - Clinical Event: Weight;Current Weight
+#   * Medications - Inpatient - Prompt
+#       - Medication (Generic): norepinephrine
+
+meds_norepi <- read_data(dir_raw, "meds", FALSE) %>%
+    as.meds_inpt() %>%
+    calc_runtime() %>%
+    summarize_data()
+
+raw_weights <- read_data(dir_raw, "events-weight", FALSE) %>%
+    as.events(order_var = FALSE) 
+
